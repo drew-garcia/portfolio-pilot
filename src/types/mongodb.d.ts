d59ca28ba01ae7cd9e934cc3aca1bc54c73f73cc
Andrew Garcia <github@drewgarcia.dev>
2023-02-03 00:05:43 -0500

    fixed MongoClientOptions

--- src/types/mongodb.d.ts
+++ src/types/mongodb.d.ts
@@ -3,3 +3,9 @@ import { MongoClient } from 'mongodb';
 declare global {
   var _mongoClientPromise: Promise<MongoClient>;
 }
+
+declare module 'mongodb' {
+  interface MongoClientOptions {
+    [key: string]: any;
+  }
+}
