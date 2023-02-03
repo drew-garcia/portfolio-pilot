7457fd04907c6b0e97bb1220fbb3a8b4c8bb8943
Andrew Garcia <github@drewgarcia.dev>
2023-02-03 00:10:21 -0500

    removed global

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -17,11 +17,11 @@ if (!process.env.MONGODB_URI) {
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!_mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect() as Promise<MongoClient>;
+    _mongoClientPromise = client.connect() as Promise<MongoClient>;
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = _mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
